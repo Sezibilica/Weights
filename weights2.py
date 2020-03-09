c644ea952b923cefeaaaa83d90ff389795b9d5fa
Sezibilica <61728434+Sezibilica@users.noreply.github.com>
2020-03-09 16:58:45 +0300

    Add files via upload
    Monday 9th March

---
+++ weights2.py
@@ -0,0 +1,8 @@
+result = input('Enter your results in the format "1 1 A A A"').split( )
+
+#for i in result:
+  #  print(i)
+
+print(sorted(result))
+legend = {"A" : 6, "B" : 5, "C": 4, "D" : 3, "E" : 2, "O" : 1, "F" : 0}
+
