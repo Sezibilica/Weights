a53bf98c0d7a18e0749a9553ed120f2c81821125
Sezibilica <esthermutenyo@gmail.com>
2020-03-11 23:48:23 +0300

    Late night speculations that proved null...

--- weights2.py
+++ weights2.py
@@ -6,3 +6,8 @@ result = input('Enter your results in the format "1 1 A A A"').split( )
 print(sorted(result))
 legend = {"A" : 6, "B" : 5, "C": 4, "D" : 3, "E" : 2, "O" : 1, "F" : 0}
 
+for i in result:
+    while result[]<=1:
+        weight = result*.05
+
+#click.prompt(text, default=None, hide_input=False, confirmation_prompt=False, type=None, value_proc=None, prompt_suffix=': ', show_default=True, err=False, show_choices=True)
